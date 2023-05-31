3879741402f7a2ae394451984c045ee0a50e24c0
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-05-31 14:33:53 -0500

    Correct date float

--- variables.rb
+++ variables.rb
@@ -10,9 +10,17 @@ favorite_number = 21
 #Floats
 time = 2.28
 fraction = 1.5
-date = 05.23
+date = 5.23
 
 #Booleans
 paid_deposit = true
 in_love = false
 still_hungry = true
+
+name = "Johnny Tsunami"
+city = "Austin, Texas"
+weather = "snowy"
+
+puts name
+puts city
+puts weather
